f36f94ca896dfedeccf723d84ae75830e2f3d4f4
yersultan056 <230129@astanait.edu.kz>
2023-12-24 16:57:43 +0600

    Implement the Point class in Point.java with the following methods and fill source.txt file with 10 random coordinates

--- src/models/Point.java
+++ src/models/Point.java
@@ -1,4 +1,25 @@
 package models;
 
 public class Point {
+    private double x;
+    private double y;
+
+    //Constructor to initialize a point with given x and y coordinates.
+    public Point(){
+        this.x = x;
+        this.y = y;
+    }
+
+    //Calculate and return the distance from the current point to the destination point.
+    public double distance(Point dest) {
+        double dx = this.x - dest.x;
+        double dy = this.y - dest.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    //Provide a string representation of the point in the format
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
 }
